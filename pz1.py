bae55e231d76eb8ff724a21dab5655956bbdfc61
KateSt <89999131+KateySt@users.noreply.github.com>
2025-03-10 07:13:49 +0200

    Update pz1.py

--- pz1.py
+++ pz1.py
@@ -1,18 +1,14 @@
 import tkinter as tk
 
-# Функція для обробки натискання кнопки
 def on_button_click(message):
     label.config(text=message)
 
-# Створення основного вікна
 root = tk.Tk()
 root.title("HMI-інтерфейс")
 
-# Створення мітки
 label = tk.Label(root, text="Натисніть кнопку", font=("Arial", 14))
 label.pack(pady=10)
 
-# Створення кнопок
 button1 = tk.Button(root, text="Кнопка 1", command=lambda: on_button_click("Ви натиснули кнопку 1"))
 button1.pack(pady=5)
 
@@ -22,5 +18,4 @@ button2.pack(pady=5)
 button3 = tk.Button(root, text="Кнопка 3", command=lambda: on_button_click("Ви натиснули кнопку 3"))
 button3.pack(pady=5)
 
-# Запуск головного циклу
 root.mainloop()
