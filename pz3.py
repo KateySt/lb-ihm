e289a9c7c4327b0a4e09fadeaf994b18a1d15a9a
KateSt <89999131+KateySt@users.noreply.github.com>
2025-03-20 07:58:20 +0200

    Update pz3.py

--- pz3.py
+++ pz3.py
@@ -3,27 +3,21 @@ from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout
 class MyApp(QWidget):
     def __init__(self):
         super().__init__()
-
-        # Ініціалізація елементів управління
         self.initUI()
 
     def initUI(self):
-        # Створення кнопки
         self.button = QPushButton('Натисни мене', self)
         self.button.clicked.connect(self.on_click)
 
-        # Створення вертикального макету
         vbox = QVBoxLayout()
         vbox.addWidget(self.button)
 
         self.setLayout(vbox)
 
-        # Налаштування основного вікна
         self.setWindowTitle('Мій простий інтерфейс')
         self.setGeometry(300, 300, 300, 200)
 
     def on_click(self):
-        # Зміна кольору фону вікна
         self.setStyleSheet("background-color: lightblue;")
 
 if __name__ == '__main__':
